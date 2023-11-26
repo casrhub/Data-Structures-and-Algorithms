e23c92623652b951df6085441752e10243feac48
Your Name <A01254679@tec.mx>
2023-11-26 03:51:14 -0700

    Esto es lo que va hacer up de feria

---
+++ act_5.1/ACT_5.1_HASHING.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "HASH_TABLE.cpp"
+#include "HASH_TABLE.h"
+
+int main() {
+    HASH_TABLE table1(10), table2(10);
+    
+    int hash_nums[] = {5,16,14,78,52,50,36,58,10};
+    //14,32,16,18,52,20,30
+
+    for (int num : hash_nums) {
+        table1.quadratic(num);
+    }
+    std::cout<<"--MODIFIED HASH TABLE 1 WITH QUADRATIC STRATEGY ---"<<std::endl;
+    table1.disp_tab();
+
+    for (int num : hash_nums) {
+        table2.chain(num);
+    }
+    std::cout<<"--MODIFIED HASH TABLE 2 WITH CHAIN STRATEGY ---"<<std::endl;
+    table2.disp_tab();
+
+    return 0;
+}
